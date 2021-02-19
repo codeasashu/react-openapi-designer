e9d9f03a3d9864981b1cf949783940edf56bcdad
Ashutosh Chaudhary <216.ashutosh@gmail.com>
2021-02-20 00:58:04 +0530

    Updated webpack

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -1,14 +1,19 @@
 const path = require('path');
+const webpack = require('webpack')
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: {index: './index.js'},
   target: 'web',
   output: {
-    path: path.resolve(__dirname, '../dist'),
     filename: '[name].js',
     sourceMapFilename: '[name].js.map',
     library: 'insomniaComponents',
     libraryTarget: 'commonjs2',
+    chunkFilename: '[id].[hash:8].js',
+  },
+  resolve: {
+    extensions: ['.js', '.jsx', '.json']
   },
   externals: ['react', 'styled-components'],
   module: {
@@ -22,4 +27,16 @@ module.exports = {
       },
     ],
   },
+  plugins: [
+    new webpack.SourceMapDevToolPlugin({
+      filename: '[file].map[query]'
+    }),
+    new HtmlWebpackPlugin({template: 'dist/index.html', inject: 'body'}),
+  ],
+  optimization: {
+    minimize: true,
+    splitChunks: {
+      chunks: 'all',
+    },
+  }
 };
--- webpack/webpack.dev.js
+++ webpack/webpack.dev.js
@@ -1,7 +1,13 @@
+const path = require('path');
 const { merge } = require('webpack-merge');
 const common = require('./webpack.common');
 
 module.exports = merge(common, {
   mode: 'development',
   devtool: 'inline-source-map',
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    compress: false,
+    port: 8080,
+  },
 })
